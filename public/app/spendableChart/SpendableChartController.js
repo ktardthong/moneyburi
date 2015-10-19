654f90c7127f0a88515aea98ab23c235d7006fcf
K Tardthong <kantatorn.tardthong@gmail.com>
2015-10-19 00:24:42 -0500

    added directive for the doughnut spendable
    App\spendableChart\tpl_dailySpendableChart

--- public/app/spendableChart/SpendableChartController.js
+++ public/app/spendableChart/SpendableChartController.js
@@ -65,13 +65,64 @@ app.controller('spendableChartController', function($scope, $http,$window) {
     };
 
 
+    $scope.showTodaySpending = function(){
+        $http.get("/todaySpending")
+            .success(function(response) {
+                $scope.d_spendable     = response[0]["d_spendable"];
+                $scope.todaySpending   = response[0]["todaySpending"];
+                $scope.todaySpendable  = $scope.d_spendable - $scope.todaySpending;
+
+                if($scope.todaySpendable<0){ $scope.todaySpendable =0}
+                //Chart
+                var json = {
+                    //"series": ["SeriesA"],
+                    "data": [$scope.todaySpending, $scope.todaySpendable],
+                    "labels":   ["Spent", "Spendable"],
+                    "colours":  ["#8D8D8D","#87D2DA"],
+                    "option": {
+                        responsive: true,
+                        maintainAspectRatio: true,
+
+                        //Boolean - Whether we should show a stroke on each segment
+                        segmentShowStroke : true,
+
+                        //String - The colour of each segment stroke
+                        segmentStrokeColor : "#fff",
+
+                        //Number - The width of each segment stroke
+                        segmentStrokeWidth : 2,
+
+                        //Number - The percentage of the chart that we cut out of the middle
+                        percentageInnerCutout : 80, // This is 0 for Pie charts
+
+                        //Number - Amount of animation steps
+                        animationSteps : 100,
+
+                        //Boolean - Whether we animate the rotation of the Doughnut
+                        animateRotate : true
+                    }
+
+
+
+                };
+                $scope.spendableDough = json;
+            });
+    };
+
     //By default show the last 7 transaction
     $scope.showSpendableWeek();
-
+    $scope.showTodaySpending();
 })
 .directive('spendableChart', function() {
     return {
         restrict: 'E',
         templateUrl: '/app/spendableChart/tpl_spendableChart.html'
+    };
+})
+
+.directive('dailySpendableChart', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/app/spendableChart/tpl_dailySpendableChart.html'
     };
 });
